9a81247f0f0f98ab09b8a5f9708515fd05484b3c
chrisf2503 <christopher.flores62@myhunter.cuny.edu>
2021-11-15 10:02:25 -0500

    Changed

--- unindent.cpp
+++ unindent.cpp
@@ -5,9 +5,12 @@
 std::string removeLeadingSpances(std::string line){
     std::string new_word = "";
     for (int i = 0; i < line.length(); i++) {
-        if (isspace(line[i])){
-            new_word += line[i];
+        //This line will check if it hits a letter, character, num, etc. 
+        if(!isspace(line[i])){
+            //This line will give you a substring from the current index up to the end of the line.
+            newLine = line.substr(i,(line.length() - i));
+			break;//This will simply stop the loop, meaning once it hit this statment, then it will leave the loop
+        }
     }
     return new_word;
-    }
 }
